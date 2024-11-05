c0221b79bd9151e64972a44d617578f4e376e584
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-05 23:43:27 +0900

    Time: 1 ms (95.08%), Space: 44.9 MB (94.2%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,42 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        /*
-        leftmost pivot index
-        leftsum == rightsum -> index
-        if no -> -1
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
 
-        data structure : array
-        prefix sum
-        [1, 8, 11, 17, 22, 28]
-        result = 3(17)
-        leftsum = currentIdx
-        rightsum = lastIdx - (currentIdx - 1)
-
-        if : left == right ? left -> range sum :  arr[j] - arr[i - 1](right - left)
-        if : 
-        [2, 3, 2]
-
-        [-1, -2, -3, -2, -1, 0]
-        [-1, -2, -1, 0, 0, 0]
-         */
-
-         int[] prefixSum = new int[nums.length];
-         prefixSum[0] = nums[0];
-         
-         for (int i = 1; i < prefixSum.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-         }
-
-         if (prefixSum[0] == prefixSum[prefixSum.length - 1]) {
-            return 0;
-         }
-
-         for (int i = 1; i < prefixSum.length; i++) {
-            if (prefixSum[i] == (prefixSum[prefixSum.length - 1] - prefixSum[i - 1])) {
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = sum - leftSum - nums[i];
+            if (leftSum == rightSum) {
                 return i;
             }
-         }
-
-         return -1;
+            
+            leftSum += nums[i];
+        }
+        
+        return -1;
     }
 }
