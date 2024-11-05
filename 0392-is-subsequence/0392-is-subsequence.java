e8aac5e026685d94ef7e1371b77850f3d04fcea0
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-06 02:10:00 +0900

    Time: 2 ms (61.75%), Space: 41.3 MB (57.2%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        /**
-        appear in order
-        two pointer
-        if find s pointer ++
-        pointer == t.length() - 1 retrun true;
-         */
-         int pointer = 0;
+         int sPointer = 0, tPointer = 0;
 
-         for (int i = 0; i < t.length(); i++) {
-            if (pointer < s.length() && s.charAt(pointer) == t.charAt(i)) pointer++;
-            if (pointer == s.length()) return true;
-         }
+        while (sPointer < s.length() && tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+            }
+            tPointer++;
+        }
 
-         return pointer == s.length();
+         return sPointer == s.length();
     }
 }
