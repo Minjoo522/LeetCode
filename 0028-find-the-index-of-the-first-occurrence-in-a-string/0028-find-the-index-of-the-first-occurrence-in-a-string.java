27b4eb6ed2ae4f81da795d035e2176c08a84c584
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-12 11:42:53 +0900

    Time: 0 ms (100%), Space: 41.8 MB (8.48%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,11 +1,9 @@
 class Solution {
   public int strStr(String haystack, String needle) {
-    String tmp = haystack;
-    for (int i = 0; i <= haystack.length() - 1; i++) {
-      if (tmp.startsWith(needle)) {
+    for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+      if (haystack.substring(i, i + needle.length()).equals(needle)) {
         return i;
       }
-      tmp = haystack.substring(i + 1);
     }
     return -1;
   }
