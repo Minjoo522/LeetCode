f0fff56b063bdc60f6b54cb3a3b617bc628e555d
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-15 21:44:53 +0900

    Time: 0 ms (100%), Space: 40.7 MB (60.28%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,20 +1,7 @@
-import java.util.*;
-
 class Solution {
     public int addDigits(int num) {
-    int result = num;
-
-    while (result >= 10) {
-      int tmp = 0;
-      String strNum = String.valueOf(result);
-
-      for (int i = 0; i < strNum.length(); i++) {
-        tmp += Character.getNumericValue(strNum.charAt(i));
-      }
-
-      result = tmp;
-    }
-
-    return result;
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
   }
 }
